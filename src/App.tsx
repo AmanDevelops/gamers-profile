0c8c8c38c446d7842c6b332690420eef5d52c98d
AmanDevelops <amanpal2072004@gmail.com>
2025-07-01 18:32:53 +0530

    refactor: streamline routing and suspense handling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import ProfileSuccess from "./pages/ProfileSuccess";
 
+const Home = lazy(() => import("./pages/Home"));
+const Profile = lazy(() => import("./pages/Profile"));
+const ProfileSuccess = lazy(() => import("./pages/ProfileSuccess"));
 const ViewProfile = lazy(() => import("./pages/ViewProfile"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-profile" element={<Profile />} />
-        <Route
-          path="/profile/:username"
-          element={
-            <Suspense fallback={<div>Loading profile...</div>}>
-              <ViewProfile />
-            </Suspense>
-          }
-        />
-        <Route path="/profile-success" element={<ProfileSuccess />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-profile" element={<Profile />} />
+          <Route path="/profile-success" element={<ProfileSuccess />} />
+          <Route path="/profile/:username" element={<ViewProfile />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
